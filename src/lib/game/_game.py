0e34e0c7da5982014c120616a8230a212ddc5b43
mikayla-anne <61417406+mikayla-anne@users.noreply.github.com>
2021-10-24 19:24:04 +0200

    om v om

--- src/lib/game/_game.py
+++ src/lib/game/_game.py
@@ -10,10 +10,10 @@ import csv
 import numpy as np
 
 
-csv_save_a = open('acts_om1.csv', 'w', encoding='UTF8', newline='')
+csv_save_a = open('acts_om5.csv', 'w', encoding='UTF8', newline='')
 writer_a = csv.writer(csv_save_a)
 
-csv_save_b = open('check_om1.csv', 'w', encoding='UTF8', newline='')
+csv_save_b = open('check_om5.csv', 'w', encoding='UTF8', newline='')
 writer_b = csv.writer(csv_save_b)
 
 
@@ -65,7 +65,7 @@ class Game:
 
         num_games = 5000
 
-        csv_save = open('savingprobsg_om1.csv', 'w', encoding='UTF8', newline='')
+        csv_save = open('savingprobsg_om5.csv', 'w', encoding='UTF8', newline='')
         writer = csv.writer(csv_save)
 
         blue_score = 0
